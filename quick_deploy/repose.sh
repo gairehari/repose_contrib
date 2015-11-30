8cf1cdd93770d2b33659e60220315401e5ea9f06
Hari Gaire <hariprasad.gaire@rackspace.com>
2015-11-30 10:36:08 -0600

    updated repose installation script to follow redirects when using curl; removed not working stop/restart codes

--- quick_deploy/repose.sh
+++ quick_deploy/repose.sh
@@ -14,13 +14,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 start_repose()
 {
     echo "Starting repose..."
-    java -jar $REPOSE/repose-valve.jar START -s 8888 -c $CONFIG_DIR
-}
-
-stop_repose()
-{
-    echo "Stopping repose..."
-    java -jar $REPOSE/repose-valve.jar STOP -s 8888
+    java -jar $REPOSE/repose-valve.jar -c $CONFIG_DIR -p 8887
 }
 
 install_repose()
@@ -29,14 +23,14 @@ install_repose()
     mkdir $REPOSE $ARTIFACT_DIR $DEPLOYMENT_DIR $CONFIG_DIR $LOG_DIR
 
     LATEST_URL="http://maven.research.rackspacecloud.com/content/repositories/releases/com/rackspace/repose/installation/deb/valve/repose-valve/maven-metadata.xml"
-    
+
 # wget may be unavailable on Mac or other OS platfoms. Script falls back to curl if wget is not available
    if wget $LATEST_URL -qO-; then
         echo "using wget"
         LATEST_XML=`wget $LATEST_URL -qO-`
     else
-        echo "using curl"    
-        LATEST_XML=`curl $LATEST_URL`
+        echo "using curl"
+        LATEST_XML=`curl -L $LATEST_URL`
     fi
 
 LATEST_VERSION=`echo $LATEST_XML | sed -n -e 's/.*<release>\(.*\)<\/release>.*/\1/p'`
@@ -46,19 +40,17 @@ LATEST_VERSION=`echo $LATEST_XML | sed -n -e 's/.*<release>\(.*\)<\/release>.*/\
     echo "Latest Version: $LATEST_VERSION"
 
     echo "Downloading latest Valve and Filter Bundle..."
-    
+
      if wget $LATEST_URL -qO-; then
         echo "using wget"
          wget -nv $LATEST_BUILD_JAR -O $REPOSE/repose-valve.jar
         wget -nv $LATEST_BUILD_FB -O $ARTIFACT_DIR/filter-bundle.ear
     else
-        echo "using curl"    
-        curl $LATEST_BUILD_JAR -o $REPOSE/repose-valve.jar
-        curl $LATEST_BUILD_FB -o $ARTIFACT_DIR/filter-bundle.ear
+        echo "using curl"
+        curl $LATEST_BUILD_JAR -Lo $REPOSE/repose-valve.jar
+        curl $LATEST_BUILD_FB -Lo $ARTIFACT_DIR/filter-bundle.ear
     fi
 
-    
-   
 
     echo "Downloading config files"
 
@@ -82,16 +74,7 @@ case "$1" in
   start)
     start_repose
     ;;
-  stop)
-    stop_repose
-    ;;
-  restart)
-    stop_repose
-    sleep 5
-    start_repose
-    ;;
-
   *)
-    echo "Usage: repose.sh  {install|start|stop|restart}"
+    echo "Usage: repose.sh  {install|start}"
     exit 1
 esac
